411fa55a95bec03d141fdcb08fb29dd033158a7b
Tomasz Łukaszyk <el.moue@gmail.com>
2018-01-11 11:43:12 +0100

    Ads button to share final score

--- app/src/main/java/com/example/adnroid/comicbookquiz/Summary.java
+++ app/src/main/java/com/example/adnroid/comicbookquiz/Summary.java
@@ -1,8 +1,10 @@
 package com.example.adnroid.comicbookquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +15,8 @@ public class Summary extends AppCompatActivity {
     TextView finalScoreView;
     //Displays an icon depending on users performance
     ImageView scoreIcon;
+    //Holds the value of final score
+    int finalScore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +25,7 @@ public class Summary extends AppCompatActivity {
         finalScoreView = findViewById(R.id.final_score);
         scoreIcon = findViewById(R.id.score_icon);
         //get the score value from main activity
-        int finalScore = getIntent().getIntExtra("score", 0);
+        finalScore = getIntent().getIntExtra("score", 0);
         //display the given score
         finalScoreView.setText(finalScore + "/6");
         //display icon depending o the score value
@@ -29,6 +33,18 @@ public class Summary extends AppCompatActivity {
             scoreIcon.setImageResource(R.mipmap.bad);
         } else if (finalScore < 5) {
             scoreIcon.setImageResource(R.mipmap.neutral);
+        }
+    }
+
+    public void shareScore (View view) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text, finalScore));
+        shareIntent.setType("text/plain");
+        String title = getString(R.string.share_with);
+        Intent chooser = Intent.createChooser(shareIntent, title);
+        if (shareIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
         }
     }
 }
